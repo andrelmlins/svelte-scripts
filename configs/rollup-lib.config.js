629b754eee43f2538b41e853aa0673a40cfd13b3
André Lins <andrelucas01@hotmail.com>
2020-03-09 15:54:45 -0300

    Error in rollup lib

--- configs/rollup-lib.config.js
+++ configs/rollup-lib.config.js
@@ -1,18 +1,21 @@
 'use strict';
 
+const fs = require('fs');
 const svelte = require('rollup-plugin-svelte');
 const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 
+const packageJson = JSON.parse(fs.readFileSync('./package.json'));
+
 const createConfig = () => [
   {
     input: 'src/lib/index.svelte',
-    output: { file: pkg.main, format: 'umd', name: 'Library' },
+    output: { file: packageJson.main, format: 'umd', name: packageJson.name },
     plugins: [svelte(), resolve(), commonjs()]
   },
   {
     input: 'src/lib/index.svelte',
-    output: { file: pkg.module, format: 'es' },
+    output: { file: packageJson.module, format: 'es' },
     external: ['svelte/internal'],
     plugins: [svelte(), commonjs()]
   }
