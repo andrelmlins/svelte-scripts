75840f3d67dc15e7c80911f4607fa8564a6489c4
André Lins <andrelucas01@hotmail.com>
2020-03-09 15:19:44 -0300

    Inserting template library

---
+++ template-library/src/lib/index.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let count = 0;
+</script>
+
+<style>
+  .button {
+    border: none;
+    box-shadow: none;
+    background-color: #00838f;
+    color: #fff;
+    padding: 0px 20px;
+    font-size: 20px;
+    height: 40px;
+  }
+</style>
+
+<button class="button" on:click={() => count++}>Clicked: {count}</button>
